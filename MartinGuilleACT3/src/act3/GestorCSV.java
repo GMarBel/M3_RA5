26a69a48e012ffcb43fc4a8cd473d865a86bd42a
Guillermo <gmaaarbel@gmail.com>
2026-04-09 20:26:51 +0200

    Resolver errores

--- MartinGuilleACT3/src/act3/GestorCSV.java
+++ MartinGuilleACT3/src/act3/GestorCSV.java
@@ -4,29 +4,73 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.Set;
 
 public class GestorCSV implements InterfaceGestorFitxers{
 
+	public Pacient toPacient(String linea) throws InvalidCodiPacientException {
+		 String[] datos = linea.trim().split(";");
+		 for (int i = 0; i < datos.length; i++) datos[i] = datos[i].trim();
+	     
+		 String nom = datos[0];
+		 String dni = datos[1];
+		 LocalDate dataNaix = LocalDate.parse(datos[2]);
+		 String telefon = datos[3];
+		 String codi = datos[4];
+		 String historial = datos[5];
+		 
+		 Pacient p = new Pacient(nom,dni,dataNaix,telefon,codi,historial);
+		 return p;
+	}
+	
+	public String toCSV(Pacient p) {
+		return p.getNom() + ";" + p.getDni() + ";" + p.getDataNaixement() + ";" + p.getTlf() + ";" + p.getCodi() + ";" + p.getHistorial();
+	}
+
+	//ORDENAR LEER Y GUARDAR
+	
 	@Override
-	public boolean desaPacients(String nomFitxer,Set<Pacient> listaPacients) {
+	public Set<Pacient> llegirPacients(String nomFitxer) throws IOException {
 		File fitxer = new File(nomFitxer);
 		try(BufferedReader bReader = new BufferedReader(new FileReader(fitxer))){
-			
+			String linea;
+			while ((linea = bReader.readLine()) != null) {
+				linea = linea.trim();
+				if (linea.startsWith("#") || linea.isEmpty()) continue;
+				Pacient p = toPacient(linea);
+				if (p != null)
+					listaPacients.add(p);
+			}
 		}catch (FileNotFoundException e) {
 			System.out.println("Fitxer no existeix");
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-		return false;
-	}
-
-	@Override
-	public void llegirPacients(String nomFitxer) {
-		// TODO Auto-generated method stub
+		
+		return null;
 		
 	}
+	
+	@Override
+	public boolean desaPacients(String nomFitxer,Set<Pacient> listaPacients) throws InvalidCodiPacientException {
+		boolean append = false;
+		try(PrintWriter pWriter = new PrintWriter(new FileWriter(nomFitxer,append))){
+			for(Pacient p:)
+			pWriter.println(toCSV(p));
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+	
+	
+
 
 
 }
--- MartinGuilleACT3/src/act3/InterfaceGestorFitxers.java
+++ MartinGuilleACT3/src/act3/InterfaceGestorFitxers.java
@@ -1,8 +1,9 @@
 package act3;
 
+import java.io.IOException;
 import java.util.Set;
 
 public interface InterfaceGestorFitxers {
-	public boolean desaPacients(String nomFitxer,Set<Pacient> listaPacients);
-	public void llegirPacients(String nomFitxer);
+	public boolean desaPacients(String nomFitxer,Set<Pacient> listaPacients) throws InvalidCodiPacientException;
+	public Set<Pacient> llegirPacients(String nomFitxer) throws IOException;
 }
